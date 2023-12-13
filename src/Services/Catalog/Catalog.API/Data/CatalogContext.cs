1813d717d69dfdd36aed9904d0e5c905781c3c0e
ninabgan <ninabgan@hotmail.no>
2023-12-13 13:40:29 +0100

    Basket API

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("Databasesettings:Connectionstring"));
+            var database = client.GetDatabase(configuration.GetValue<string>("Databasesettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("Databasesettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products {get; }
+    }
+}
